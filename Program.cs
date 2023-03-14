77949de875e0b1dc2581ce2599d4030d2a98bd21
liz0nika <borisenkoliza9@gmail.com>
2023-03-14 23:27:53 +0200

    Create Program.cs
    Find the largest and smallest dictionary values, remove them and sort the dictionary by key.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаба_1._2__2_семестр_
+{
+    internal class Program
+    {
+    
+static void Main(string[] args)
+        {
+            
+            var colors = new Dictionary<int, string>()
+            {
+                [5] = "green",
+                [3] = "blue",
+                [8] = "orange",
+                [1] = "white",
+                [2] = "black",
+                [9] = "red",
+                [4] = "violet"
+            };
+            foreach (var color in colors)
+            {
+                Console.WriteLine($"Key: {color.Key}.  Value: {color.Value}.");
+            }
+            var maxKey = colors.OrderByDescending(x => x.Key).First().Key;
+            var minKey = colors.OrderByDescending(x => x.Key).Last().Key;
+            Console.WriteLine();
+            Console.WriteLine($"The biggest key: {maxKey}");
+            Console.WriteLine($"The smallest key: {minKey}");
+            colors.Remove(maxKey);
+            colors.Remove(minKey);
+            Console.WriteLine();
+            Console.WriteLine("Dictionary after removing max and min keys: ");
+            foreach (var color in colors)
+            {
+                Console.WriteLine($"Key: {color.Key}.  Value: {color.Value}.");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sorted Dictionary by keys: ");
+           
+            var l = colors.OrderBy(key => key.Key);
+
+            var dic = l.ToDictionary((keyItem) => keyItem.Key, (valueItem) => valueItem.Value);
+            foreach (var item in dic)
+            {
+
+                Console.WriteLine($"Key: {item.Key}. Value: {item.Value}");
+            }
+            Console.ReadKey();  
+        }
+    }
+}
